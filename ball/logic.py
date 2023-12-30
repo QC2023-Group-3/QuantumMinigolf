029d58675058efd3207872de95535e410d40a3bc
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-12-30 17:17:45 +1100

    Add template code to init balls

---
+++ ball/logic.py
@@ -0,0 +1,6 @@
+from .subBall import subBall
+
+def initializeBalls(amtOfBalls: int) -> list[subBall]:
+	# Returns a list of subBalls, where the amtOfBalls is the amount of subBalls in the list
+
+	return [] # Change later
