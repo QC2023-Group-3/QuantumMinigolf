66c7296b02fbdaba04faba45550e21dc120576db
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-12-30 17:26:21 +1100

    Add pickFinal template

--- ball/logic.py
+++ ball/logic.py
@@ -4,3 +4,8 @@ def initializeBalls(amtOfBalls: int) -> list[subBall]:
 	# Returns a list of subBalls, where the amtOfBalls is the amount of subBalls in the list
 
 	return [] # Change later
+
+def pickFinal() -> subBall:
+	# Return the final subBall that is picked to determine if you have won or not
+
+	return subBall
