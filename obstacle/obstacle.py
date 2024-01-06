b596c2b655d3f3b914b731a3b60d3382215dd627
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-01-06 14:45:28 +0800

    Fixed obstacle code to follow quantum physics

--- obstacle/obstacle.py
+++ obstacle/obstacle.py
@@ -1,42 +1,24 @@
 class obstacle:
-	def __init__(self, beginX: float, beginY: float, endX: int, endY: int, balls: list) -> None:
+	def __init__(self, beginX: float, beginY: float, endX: int, endY: int, balls: list, psi: list) -> None:
 		self.beginX = beginX
 		self.beginY = beginY
 		self.endX = endX
 		self.endY = endY
 
 		self.balls = balls
-
-	def calcSide(self, ballX, ballY) -> str:
-		if self.beginX <= ballX <= self.endX and ballY == self.endY:
-			return 'N'
-		if self.beginY <= ballY <= self.endY and ballX == self.endX:
-			return 'E'
-		if self.beginX <= ballX <= self.endX and ballY == self.beginY:
-			return 'S'
-		if self.beginY <= ballY <= self.endY and ballX == self.beginX:
-			return 'W'
-
-	def calcBearing(self, oldBearing, side):
-		match side:
-			case 'N': #value entered must be 0 < x < 360
-				if oldBearing <= 90:
-					return 360 - oldBearing
-				else:
-					return 180 - oldBearing
-			case 'E': #value entered must be 0 < x < 180
-				return 180 - oldBearing
-			case 'S': #value entered must be 90 < x < 360
-				return 360 - oldBearing
-			case 'W': #value entered must be 180 < x < 360
-				if oldBearing <= 270:
-					return 540 - oldBearing
-				else:
-					return 540 - oldBearing
+		self.psi = psi
+	
+	def newPsi(self, newPsi) -> None:
+		self.psi = newPsi
+	
+	def changedPsi(self) -> list:
+		return self.psi
 	
 	def checkCollided(self):
-		for subBall in self.balls:
-			if self.beginX >= subBall.x >= self.endX and self.beginY >= subBall.y >= self.endY:
-				subBall.changeAngle(self.calcBearing(subBall.angle, self.calcSide(subBall.x, subBall.y)))
+		for y in range(len(self.psi)):
+			for x in range(len(y)):
+				if self.beginY >= y >= self.endY and self.beginX >= x >= self.endX:
+					self.psi[y][x] = 0
 
+		return self.psi
 		
