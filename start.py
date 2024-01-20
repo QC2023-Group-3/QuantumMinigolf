a7f34e39c7fa683416661d64b0d3afccec880f6b
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-01-20 16:18:10 +0800

    Add some comments

--- start.py
+++ start.py
@@ -2,10 +2,11 @@ from manim import * # Animation Engine
 import pyglet # User Input
 from pyglet.window import key as pyglet_key # Keyboard Input
 
+# Import mathematical logic behind game
 from QuantumAPI.ball import ball
 from QuantumAPI.obstacle import obstacle
 
-
+# MAIN
 class QuantumMinigolf(Scene):
 	def construct(self):
 		# Define Variables
