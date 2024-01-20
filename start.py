185c70189300cc490cc9f4d78f6fd938ea81a575
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-01-20 16:11:43 +0800

    Add base code

--- start.py
+++ start.py
@@ -1,1 +1,12 @@
-import manim
+from manim import * # Animation Engine
+import pyglet # User Input
+from pyglet.window import key as pyglet_key
+
+from QuantumAPI.ball import ball
+from QuantumAPI.obstacle import obstacle
+
+
+class QuantumMinigolf(Scene):
+	def construct(self):
+		# Construct
+		pass
