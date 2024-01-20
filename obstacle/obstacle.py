624206b2f5fc440927e5e4641d5e623d7430a035
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-01-20 19:09:07 +0800

    Fix useless need for psi

--- obstacle/obstacle.py
+++ obstacle/obstacle.py
@@ -1,24 +1,15 @@
 class obstacle:
-	def __init__(self, beginX: float, beginY: float, endX: int, endY: int, balls: list, psi: list) -> None:
+	def __init__(self, beginX: float, beginY: float, endX: int, endY: int) -> None:
 		self.beginX = beginX
 		self.beginY = beginY
 		self.endX = endX
 		self.endY = endY
-
-		self.balls = balls
-		self.psi = psi
 	
-	def newPsi(self, newPsi) -> None:
-		self.psi = newPsi
-	
-	def changedPsi(self) -> list:
-		return self.psi
-	
-	def checkCollided(self):
-		for y in range(len(self.psi)):
+	def checkCollided(self, psi):
+		for y in range(len(psi)):
 			for x in range(len(y)):
 				if self.beginY >= y >= self.endY and self.beginX >= x >= self.endX:
-					self.psi[y][x] = 0
+					psi[y][x] = 0
 
-		return self.psi
+		return psi
 		
