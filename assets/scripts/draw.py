b31c1a2ea4f27439fbcb741633b0b52277f80dd6
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-01-20 18:05:56 +0800

    Add code to draw the ball

--- assets/scripts/draw.py
+++ assets/scripts/draw.py
@@ -13,3 +13,14 @@ def drawObstacle(surface, obstacles) -> None:
 		height = abs(obstacle.beginY-obstacle.endY)
 
 		pygame.draw.rect(surface, COLORS["obstacle"], pygame.Rect(left, top, width, height))
+
+# Draw the ball
+def drawBall(surface, ball, particleWidth) -> None:
+	psi = ball.psi
+	# Converting psi/matrix to a surface
+	for y in psi:
+		for x in y:
+			currColor = int(x*255)
+			color = [i*currColor for i in COLORS["ballHeatmap"]] # Select which RGB values should be colored
+
+			pygame.draw.rect(surface, color, (x-(particleWidth/2), y-(particleWidth/2), particleWidth, particleWidth))
